e342081972d2b1e494f3f43014c5fa2e72aaf9cd
Luís Pinto <luispinto23@gmail.com>
2022-09-30 23:57:14 +0100

    feat: added update product handler

--- data/products.go
+++ data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -66,4 +67,30 @@ func getNextID() int {
 	lp := productList[len(productList)-1]
 
 	return lp.ID + 1
+}
+
+func UpdateProduct(id int, p *Product) error {
+	p, pos, err := findProduct(id)
+
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+
+	productList[pos] = p
+
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+
+	return nil, -1, ErrProductNotFound
 }
--- handlers/products.go
+++ handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/luispinto23/go-micro/data"
 )
@@ -27,7 +29,30 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == http.MethodPut {
-		p.updateProducts(rw, r)
+		//get the id from the uri
+		reg := regexp.MustCompile(`/([0-9]+)`)
+
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+
+		if err != nil {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		p.updateProducts(id, rw, r)
 		return
 	}
 
@@ -36,6 +61,8 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 }
 
 func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Product")
+
 	lp := data.GetProducts()
 
 	err := lp.ToJSON(rw)
@@ -45,6 +72,8 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 }
 
 func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
 	prod := &data.Product{}
 
 	err := prod.FromJSON(r.Body)
@@ -55,10 +84,26 @@ func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 	data.AddProduct(prod)
 }
 
-func (p *Products) updateProducts(rw http.ResponseWriter, r *http.Request) {
-	// get request Body
+func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Product")
 
-	// get Products from request Body
-	p.l.Println("Handle PUT request")
+	prod := &data.Product{}
 
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "Can't decode request body", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
 }
